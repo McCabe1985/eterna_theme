7f4ddf2ebdb443b023bd3c4f00a94e914626aef9
Patrick McCabe <pmccabe.dev@gmail.com>
2023-01-31 09:34:36 +0000

    work in progress on navbar and hamburger menu

--- functions.php
+++ functions.php
@@ -3,6 +3,7 @@
 function eterna_files()
 {
     wp_enqueue_style('eterna_stylesheet', get_theme_file_uri('/assets/styles/styles.css'));
+    wp_enqueue_script('eterna_hamburger', get_theme_file_uri('/assets/js/hamburger.js'));
 }
 
 add_action('wp_enqueue_scripts', 'eterna_files');
--- header.php
+++ header.php
@@ -2,14 +2,16 @@
 <html lang="en">
 
 <head>
-    <?php wp_head(); ?>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> <?php wp_head(); ?>
 </head>
 
 <body>
     <nav class="nav-container">
-        <div class="nav-logo-wrapper">
-
-        </div>
+        <a href="/" class="nav-logo-wrapper">
+            <img src="<?php echo get_theme_file_uri("/assets/images/svg/placeholder_white.svg"); ?>" height="38.7" width="38.7" alt="logo" class="nav-logo">
+        </a>
 
         <div class="nav-links-wrapper">
             <a href="/" class="nav-link">LINK</a>
@@ -17,4 +19,15 @@
             <a href="/" class="nav-link">LINK</a>
             <a href="/" class="nav-link">LINK</a>
         </div>
+
+        <button class="hamburger-button">
+            <img src="<?php echo get_theme_file_uri("/assets/images/svg/hamburger.svg"); ?>" height="14" width="14" alt="hamburger" class="hamburger-icon">
+        </button>
+
+    </nav>
+    <nav class="hamburger-menu">
+        <a href="/" class="hamburger-link">LINK</a>
+        <a href="/" class="hamburger-link">LINK</a>
+        <a href="/" class="hamburger-link">LINK</a>
+        <a href="/" class="hamburger-link">LINK</a>
     </nav>
