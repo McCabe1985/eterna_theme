05a9f6fd6e192b1f767f764144319dc7eb03497a
Patrick McCabe <pmccabe.dev@gmail.com>
2023-01-31 15:00:16 +0000

    finish hero nad tweak mobile nav

--- front-page.php
+++ front-page.php
@@ -2,6 +2,7 @@
 <main class="site-content-container">
     <?php
     get_template_part('template-parts/homepage/hero');
+    get_template_part('template-parts/homepage/services');
     ?>
 
 </main>
