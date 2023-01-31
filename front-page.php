287479843149235443e2544b3351a2f769251fdb
Patrick McCabe <pmccabe.dev@gmail.com>
2023-01-31 16:47:39 +0000

    sections three and four

--- front-page.php
+++ front-page.php
@@ -4,6 +4,8 @@
     get_template_part('template-parts/homepage/hero');
     get_template_part('template-parts/homepage/section-one');
     get_template_part('template-parts/homepage/section-two');
+    get_template_part('template-parts/homepage/section-three');
+    get_template_part('template-parts/homepage/section-four');
     ?>
 
 </main>
