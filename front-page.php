bea726b0d91a71828508855658c45fecba470e75
Patrick McCabe <pmccabe.dev@gmail.com>
2023-01-31 15:42:40 +0000

    implement "section-one" layout

--- front-page.php
+++ front-page.php
@@ -2,7 +2,7 @@
 <main class="site-content-container">
     <?php
     get_template_part('template-parts/homepage/hero');
-    get_template_part('template-parts/homepage/services');
+    get_template_part('template-parts/homepage/section-one');
     ?>
 
 </main>
